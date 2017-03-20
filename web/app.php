d5632ccc2de01bbad69e0c477f17f1b26f3bac22
Marcus Haase <mail@marcus.haase.name>
2017-03-20 20:56:36 +0100

    added init code

--- web/app.php
+++ web/app.php
@@ -18,7 +18,54 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-require __DIR__.'/../src/bootstrap.php';
+ini_set('display_errors', 0);
+ini_set('xdebug.overload_var_dump', 0);
+ini_set('xdebug.var_display_max_depth', 10);
+ini_set('html_errors', 0);
+
+error_reporting(E_ALL);
+
+mb_internal_encoding('UTF-8');
+header("Content-Type: text/html; charset=UTF-8");
+
+if (ini_get('session.auto_start') == 1) {
+    die('Please disable session.autostart for this to work.');
+}
+
+// set scale for bcmath
+bcscale(6);
+
+define("MINUTE", 60);
+define("HOUR", MINUTE * 60);
+define("DAY", HOUR * 24);
+define("WEEK", DAY * 7);
+
+define("DB_ADDRESSFIELDS", 'cust_id, cust_no, cust_email, cust_corp, cust_name, cust_street, cust_zip, cust_town, cust_phone, cust_cellphone, cust_fax, cust_country, cust_group, cust_active, cust_emailverified, cust_tosaccepted, cust_cancellationdisclaimeraccepted');
+define("DB_ITEMFIELDS", 'itm_no, itm_name, itm_price, itm_vatid, itm_rg, itm_img, itm_group, itm_data, itm_weight, itml_name_override, itml_text1, itml_text2, itm_index');
+define("DB_ITEMGROUPFIELDS", 'itmg_no, itmg_name, itmg_img, itmgt_shorttext, itmgt_details');
+
+define("PATH_BASEDIR", __DIR__.'/../');
+define("PATH_DOCROOT", PATH_BASEDIR.'web/');
+
+define("PATH_CACHE", PATH_BASEDIR.'cache/');
+define("DIRNAME_TEMPLATECACHE", 'templates');
+define("PATH_TEMPLATECACHE", PATH_CACHE.DIRNAME_TEMPLATECACHE);
+define("PATH_PURIFIERCACHE", PATH_CACHE.'htmlpurifier/');
+define("DIRNAME_GLIDECACHE", 'glide');
+define("PATH_GLIDECACHE", PATH_CACHE.DIRNAME_GLIDECACHE);
+
+define("PATH_LOGS", PATH_BASEDIR.'hcsflogs/');
+define("FILE_PAYPALLOG", 'ipnlog.txt');
+
+const ENTITY_CUSTOMER = 'HaaseIT\HCSF\Entities\Customer\Customer';
+const ENTITY_USERPAGE_LANG = 'HaaseIT\HCSF\Entities\UserpageLang';
+const ENTITY_USERPAGE_BASE = 'HaaseIT\HCSF\Entities\UserpageBase';
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$HCSF = new \HaaseIT\HCSF\HCSF();
+
+$P = $HCSF->init();
 
 $aP = \HaaseIT\HCSF\Helper::generatePage($serviceManager, $P);
 
